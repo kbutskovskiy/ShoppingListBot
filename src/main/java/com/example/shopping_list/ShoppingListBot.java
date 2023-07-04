561f312ba7d28b012de9bba54d7769e40b2e53bd
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-07-05 00:33:36 +0300

    button added

--- src/main/java/com/example/shopping_list/ShoppingListBot.java
+++ src/main/java/com/example/shopping_list/ShoppingListBot.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Здесь будет проходить всё взаимодействие с ботом.
+ */
 @Component
-public class ShoppingListBot extends TelegramLongPollingBot{
+public class ShoppingListBot extends TelegramLongPollingBot {
 
     private final ConfigurationBot config;
 
@@ -30,18 +39,53 @@ public class ShoppingListBot extends TelegramLongPollingBot{
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage()) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage sendMessage = new SendMessage();
-            sendMessage.setText("Иди нахуй");
-            sendMessage.setChatId(update.getMessage().getChatId());
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+
+            if (update.getMessage().getText().equals("/start")) {
+                sendMessage.setText("Нажми на одну из кнопок ниже.");
+
+                // Создание инлайн-клавиатуры
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+                List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+                // Добавление кнопки "Старт" в инлайн-клавиатуру
+                InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+                inlineKeyboardButton.setText("Супермаркет");
+                inlineKeyboardButton.setCallbackData("Супермаркет");
+                rowInline.add(inlineKeyboardButton);
+
+                rowsInline.add(rowInline);
+                markupInline.setKeyboard(rowsInline);
+                sendMessage.setReplyMarkup(markupInline);
+            } else {
+                sendMessage.setText("Круто!");
+            }
+
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {
                 throw new RuntimeException(e);
             }
         }
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setText("Иди нахуй");
-        sendMessage.setChatId(update.getMessage().getChatId());
+
+        /**
+         * Нажали на кнопку, обработка нажатия на кнопку. В инлайн-клавиатуре
+         * callback методы, которые по-своему называются. Например "Супермаркет". Каждый нужно обработать отдельно.
+         */
+        if (update.hasCallbackQuery()) {
+            if (Objects.equals(update.getCallbackQuery().getData(), "Супермаркет")) {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(update.getCallbackQuery().getMessage().getChatId());
+                sendMessage.setText("Вы перешли в раздел супермаркет");
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/com/example/shopping_list/repository/Supermarket.java
@@ -0,0 +1,4 @@
+package com.example.shopping_list.repository;
+
+public class Supermarket {
+}
