6be3e8aebfa86e86c4a902d9f9d2bce3995b032e
Kirill Buckovsky <kbutskovskiy@mail.ru>
2023-07-08 19:29:04 +0300

    refactoring to switch-case

--- src/main/java/com/example/shopping_list/ShoppingListBot.java
+++ src/main/java/com/example/shopping_list/ShoppingListBot.java
@@ -16,7 +16,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Здесь будет проходить всё взаимодействие с ботом.
@@ -94,72 +93,74 @@ public class ShoppingListBot extends TelegramLongPollingBot {
 
         if (update.hasCallbackQuery()) {
             Long chatId = update.getCallbackQuery().getMessage().getChatId();
-            if (update.getCallbackQuery().getData().equals("Добавить покупку")) {
-                SendMessage messageToUser = workWithMessage.createMessageForSend("Выбери тип магазина", chatId);
+            switch (update.getCallbackQuery().getData()) {
+                case ("Добавить покупку") -> {
+                    SendMessage messageToUser = workWithMessage.createMessageForSend("Выбери тип магазина", chatId);
 
-                // Создание инлайн-клавиатуры
-                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-                List<InlineKeyboardButton> rowInline = new ArrayList<>();
+                    // Создание инлайн-клавиатуры
+                    InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                    List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+                    List<InlineKeyboardButton> rowInline = new ArrayList<>();
 
-                rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Супермаркет", "Супермаркет"));
+                    rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Супермаркет", "Супермаркет"));
 
-                rowsInline.add(rowInline);
-                markupInline.setKeyboard(rowsInline);
-                messageToUser.setReplyMarkup(markupInline);
-                try {
-                    execute(messageToUser);
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
+                    rowsInline.add(rowInline);
+                    markupInline.setKeyboard(rowsInline);
+                    messageToUser.setReplyMarkup(markupInline);
+                    try {
+                        execute(messageToUser);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                    previousChoice = null;
                 }
-                previousChoice = null;
-            }
 
-            if (Objects.equals(update.getCallbackQuery().getData(), "Супермаркет")) {
-                SendMessage messageToUser = workWithMessage.createMessageForSend("Введите, что нужно купить", chatId);
-                this.previousChoice = update.getCallbackQuery().getData();
-                try {
-                    execute(messageToUser);
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
+                case ("Супермаркет") -> {
+                    SendMessage messageToUser = workWithMessage.createMessageForSend("Введите, что нужно купить", chatId);
+                    this.previousChoice = update.getCallbackQuery().getData();
+                    try {
+                        execute(messageToUser);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }
 
-            if (update.getCallbackQuery().getData().equals("Посмотреть список покупок")) {
-                SendMessage messageToUser = workWithMessage.createMessageForSend("Хотите ли посмотреть для себя или весь список?", chatId);
+                case ("Посмотреть список покупок") -> {
+                    SendMessage messageToUser = workWithMessage.createMessageForSend("Хотите ли посмотреть для себя или весь список?", chatId);
 
-                // Создание инлайн-клавиатуры
-                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-                List<InlineKeyboardButton> rowInline = new ArrayList<>();
-                rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Для себя", "Для себя"));
-                rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Весь список", "Весь список"));
+                    // Создание инлайн-клавиатуры
+                    InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                    List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+                    List<InlineKeyboardButton> rowInline = new ArrayList<>();
+                    rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Для себя", "Для себя"));
+                    rowInline.add(inlineKeyboardService.createInlineKeyboardButton("Весь список", "Весь список"));
 
-                rowsInline.add(rowInline);
-                markupInline.setKeyboard(rowsInline);
-                messageToUser.setReplyMarkup(markupInline);
-                try {
-                    execute(messageToUser);
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
+                    rowsInline.add(rowInline);
+                    markupInline.setKeyboard(rowsInline);
+                    messageToUser.setReplyMarkup(markupInline);
+                    try {
+                        execute(messageToUser);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                    previousChoice = null;
                 }
-                previousChoice = null;
-            }
 
-            if (update.getCallbackQuery().getData().equals("Весь список") || update.getCallbackQuery().getData().equals("Для себя")) {
-                List<SupermarketItem> supermarketItemList = update.getCallbackQuery().getData().equals("Весь список") ?
-                        supermarketService.getSupermarketItemList() : supermarketService.getSupermarketItemListByUsername(update.getCallbackQuery().getFrom().getUserName());
+                case "Весь список", "Для себя" -> {
+                    List<SupermarketItem> supermarketItemList = update.getCallbackQuery().getData().equals("Весь список") ?
+                            supermarketService.getSupermarketItemList() : supermarketService.getSupermarketItemListByUsername(update.getCallbackQuery().getFrom().getUserName());
 
-                StringBuilder responseText = new StringBuilder();
-                for (SupermarketItem item : supermarketItemList) {
-                    responseText.append(item.getBuy()).append("\n");
-                }
-                SendMessage messageWithList = workWithMessage.createMessageForSend(responseText.toString(), chatId);
+                    StringBuilder responseText = new StringBuilder();
+                    for (SupermarketItem item : supermarketItemList) {
+                        responseText.append(item.getBuy()).append("\n");
+                    }
+                    SendMessage messageWithList = workWithMessage.createMessageForSend(responseText.toString(), chatId);
 
-                try {
-                    execute(messageWithList); // Отправляем сообщение
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
+                    try {
+                        execute(messageWithList); // Отправляем сообщение
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
